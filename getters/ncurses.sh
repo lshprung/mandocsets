e171faf20d04eb8302078a02ebc40dd94c86bfc8
Louie Shprung <lshprung@scu.edu>
2022-11-24 21:30:20 -0800

    Expand list of packages to look for man pages in

--- getters/ncurses.sh
+++ getters/ncurses.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env sh
 
-SOURCE="$(apt-file list ncurses-doc | grep -Eo "/usr/share/man/.*")"
+PACKAGES="ncurses-base
+ncurses-doc
+ncurses-hexedit   
+ncurses-bin
+ncurses-examples
+ncurses-term"
+
 DESTINATION="$(dirname "$0")/../ncurses/"
 
+get_source() {
+	echo "$PACKAGES" | while read -r package; do
+		apt-file list "$package" | grep -Eo "/usr/share/man/.*"
+	done
+}
+
 # Copy files to source
-echo "$SOURCE" | while read -r line; do
+get_source | while read -r line; do
 	unset CATEGORY
 	CATEGORY="$(basename "$(dirname "$line")")"
 	mkdir -p "$DESTINATION/$CATEGORY"
